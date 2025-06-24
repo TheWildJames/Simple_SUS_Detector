3952a68fd041d5209ea069bbe36259563afb914d
TheWIldJames <bins4us@hotmail.com>
2025-06-24 08:32:03 -0400

    Update SettingsScreen.kt

--- app/src/main/java/com/simplesusdetector/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/simplesusdetector/ui/screens/SettingsScreen.kt
@@ -1,5 +1,6 @@
 package com.simplesusdetector.ui.screens
 
+import android.content.Context
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
