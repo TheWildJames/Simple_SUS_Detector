f18c96d4233228a1d45edbda635719b6c3378a1d
TheWIldJames <bins4us@hotmail.com>
2025-06-24 08:14:14 -0400

    yert

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     
+    // Material Design 3 for Android (not Compose)
+    implementation("com.google.android.material:material:1.12.0")
+    
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
